b47b0fb4c14a5b9a9118e6c077f3f3cb3ba16cd6
Nicholas Orrell <orrelln@flip1.engr.oregonstate.edu>
2017-05-29 16:23:57 -0700

    skeleton of forms for create review, login, and sign up

---
+++ public/connectvarsEECS.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs340_orrelln');
+  define('DB_PASSWORD', '9469');
+  define('DB_NAME', 'cs340_orrelln');
+?>
